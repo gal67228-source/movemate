953c597005333e95f920423343f823db716b209c
GAL NIZRI <GALNIZRI@rafael.co.il>
2026-07-14 16:37:02 +0300

    MoveMate v0.1.3 Build #4 - Fix Riverpod widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:movemate/main.dart';
 
 void main() {
   testWidgets('welcome screen opens in Hebrew', (tester) async {
-    await tester.pumpWidget(const MoveMateApp());
+    await tester.pumpWidget(
+      const ProviderScope(
+        child: MoveMateApp(),
+      ),
+    );
     await tester.pumpAndSettle();
 
     expect(find.text('MoveMate'), findsOneWidget);
